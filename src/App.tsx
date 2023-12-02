8e7bbaf336f36eecd24cf0f4e5640be51037483e
NatKhomich <yalapulya94@mail.ru>
2023-12-02 14:44:38 +0600

    get data from firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {Auth} from './components/auth';
+import {db} from './config/firebase'
+import {getDocs, collection} from 'firebase/firestore'
+
+type ProductType = {
+    description: string
+    id: string
+    photo: string
+    price: number
+    title: string
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [productList, setProductList] = useState<Partial<ProductType>[]>([]);
+
+    const productCollectionRef = collection(db, 'product')
+
+    useEffect(() => {
+        const getProductList = async () => {
+            try {
+                const data = await getDocs(productCollectionRef)
+                const filteredData = data.docs.map(el => ({...el.data(), id: el.id}))
+                console.log(filteredData)
+                setProductList(filteredData)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getProductList()
+    }, [])
+
+
+    return (
+        <div className="App">
+            <Auth/>
+
+            <h1> Data from data base</h1>
+
+            <div className='lists'>
+                {productList.map(p => {
+                    return (
+                        <div className={'item'} key={p.id}>
+                            <h3>{p.title}</h3>
+                            <img style={{width: '200px'}} src={p.photo} alt="photo"/>
+                            <p>{p.description}</p>
+                            <p>Price: {p.price} руб</p>
+                        </div>
+                    )
+                })}
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,5 +1,7 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import {initializeApp} from 'firebase/app';
+import {getAuth, GoogleAuthProvider} from 'firebase/auth'
+import {getFirestore} from 'firebase/firestore'
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,5 +16,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app); //is optional
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+export const googleProvider = new GoogleAuthProvider()
+export const db = getFirestore(app)
+
+//const analytics = getAnalytics(app); //is optional
